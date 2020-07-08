6349e75669e600e14981255101263a39338f1d9d
Alex <36492876+EdissonRuiz78@users.noreply.github.com>
2020-07-08 16:52:22 -0500

    Primera funcion recibir dinero
    Se recibe una criptomoneda por medio de api se valida que este registrada y el valor en USD de la moneda, al igual se hacen validacion de usuarios

--- Interfaz.py
+++ Interfaz.py
@@ -1,20 +1,27 @@
+
 import sys
+import requests
 import tkinter as tk
+from tkinter import messagebox
+
+_ENDPOINT = "https://api.binance.com"
 
 def validarUsuario(usuario, codigo):
-	archivo = open("usuarios.txt", "r")
-
-	texto = archivo.read()
-	palabras = texto.split(";")
-	print("ESTO ES",palabras)
-
-	archivo.close()
-
+	linea = config_linea(usuario, codigo)
+	with open("usuarios.txt", "r") as fichero:
+		for f_linea in fichero:
+			if linea == f_linea:
+				Interfaz(usuario, codigo)
+				break
 
 
+def config_linea(usuario, codigo):
+	linea = "Usuario:{} Codigo:{}\n".format(usuario, codigo)
+	return linea
 
-def recibirC():
-	recibirCantidad = tk.Tk()
+
+def recibirC(codigo):
+	recibirCantidad = tk.Toplevel()
 	recibirCantidad.title("Recibir cantidad")
 	recibirCantidad.resizable(False, False)
 	recibirCantidad.iconbitmap("cripto.ico")
@@ -36,23 +43,60 @@ def recibirC():
 	textoCodigo=tk.Entry(recibirCantidad)
 	textoCodigo.grid(column=1, row=2, sticky="w", padx=20, pady=10)
 
-	botonEnviardatos = tk.Button(recibirCantidad, text="Enviar datos", font=("Arial Bold", 12))
+	botonEnviardatos = tk.Button(recibirCantidad, text="Enviar datos", command=lambda:recibirDatos(str(textoMoneda.get()), textoCantidad.get(), textoCodigo.get(), codigo), font=("Arial Bold", 12))
 	botonEnviardatos.grid(column=1, row=3, sticky="w", padx=20, pady=10, columnspan=2)
 
+	botonRegresar = tk.Button(recibirCantidad, text="Regresar", command=recibirCantidad.destroy, font=("Arial Bold", 12))
+	botonRegresar.grid(column=2, row=3, sticky="e", padx=20, pady=10)
 
-def Interfaz():
-	Sesion.state(newstate="withdraw")
-	Interfaz = tk.Tk()
+def _url(api):
+    return _ENDPOINT+api
+
+def get_price(cripto):
+    return requests.get(_url("/api/v3/ticker/price?symbol="+cripto))
+
+
+def recibirDatos(monedaR, cantidad, codigoR, codigo):
+	monedas=()
+	monedas_dict={}	
+
+	COINMARKET_API_KEY = "57c1101b-e4a3-4450-8290-8fd500f00a5a"
+	headers = {'Accepts': 'application/json', 'X-CMC_PRO_API_KEY': COINMARKET_API_KEY}
+
+	data=requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",headers=headers).json()
+
+	for cripto in data["data"]:
+		monedas_dict[cripto["symbol"]]=cripto["name"]
+
+	monedas = monedas_dict.keys()
+
+	if monedaR in monedas:
+		print("La moneda existe "+monedaR)
+	else:
+		messagebox.showwarning("Eror", "Moneda invalida no registrada en coimnmarketcap.com")
+
+	if codigo == codigoR:
+		messagebox.showwarning("Eror", "Este codigo pertenece a tu usuario")
+
+	data = get_price(monedaR+"USDT").json()
+	print("El precio de",monedaR,"en USD es:",data["price"])
+
+	
+
+
+def Interfaz(usuario, codigo):
+	Sesion.withdraw()
+	Interfaz = tk.Toplevel()
 	Interfaz.title("Mi billetera digital")
 	Interfaz.resizable(False, False)
 	Interfaz.iconbitmap("cripto.ico")
 	Interfaz.geometry("600x400+200+100")
 	Interfaz.config(cursor="hand2")
 
-	titulo = tk.Label(Interfaz, text="Bienvenido a tu billetera digital", font=("Arial Bold", 18))
-	titulo.grid(column=1, row=0, sticky="w", padx=130, pady=20, columnspan=4)
+	titulo = tk.Label(Interfaz, text="Bienvenido a tu billetera digital "+usuario, font=("Arial Bold", 18))
+	titulo.grid(column=1, row=0, sticky="w", padx=80, pady=20, columnspan=4)
 
-	botonRecibircantidad = tk.Button(Interfaz, text="Recibir cantidad", command=lambda:recibirC(), font=("Arial Bold", 12))
+	botonRecibircantidad = tk.Button(Interfaz, text="Recibir cantidad", command=lambda:recibirC(codigo), font=("Arial Bold", 12))
 	botonRecibircantidad.grid(column=1, row=3, sticky="w", padx=20, pady=10)
 
 	botonTransferirmonto = tk.Button(Interfaz, text="Transferir monto", font=("Arial Bold", 12))
@@ -67,7 +111,7 @@ def Interfaz():
 	botonMostrarhistorial = tk.Button(Interfaz, text="Mostrar historico de transacciones", font=("Arial Bold", 12))
 	botonMostrarhistorial.grid(column=1, row=7, sticky="w", padx=20, pady=10)
 
-	botonSalir = tk.Button(Interfaz, text="Salir", command=lambda:sys.exit(), font=("Arial Bold", 12))
+	botonSalir = tk.Button(Interfaz, text="Cerrar sesion", command=Interfaz.destroy, font=("Arial Bold", 12))
 	botonSalir.grid(column=1, row=9, sticky="e", padx=20, pady=10)
 
 
@@ -94,14 +138,7 @@ if __name__ == '__main__':
 	textoCodigo.grid(column=1, row=2, sticky="w", padx=20, pady=10)
 
 	botonIniciar = tk.Button(Sesion, text="Iniciar Sesion", command=lambda:validarUsuario(textoUsuario.get(), textoCodigo.get()), font=("Arial Bold", 12))
-	botonIniciar.grid(column=0, row=3, sticky="w", padx=140, pady=30, columnspan=2)
-
-	
+	botonIniciar.grid(column=0, row=3, sticky="w", padx=140, pady=20, columnspan=2)
 
 	Sesion.mainloop()
  
-
-
-
-
-
